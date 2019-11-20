95b239eb6ace44fbe1ec335d911eef1ac2eece83
Julalak Tammaphet <s6030213020@phuket.psu.ac.th>
2019-11-21 00:06:41 +0700

    add new file and edit file and delate

---
+++ server.js
@@ -0,0 +1,156 @@
+var express = require('express');
+var app = express();
+// var data = require("./StudentInformations.json");
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+var options = { useUnifiedTopology: true, useNewUrlParser: true };
+var bodyPaser = require('body-parser');
+app.use(bodyPaser.json());
+app.use(bodyPaser.urlencoded({ extended: true }));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//Home
+app.get("/", function(req, res) {
+    res.render('pages/Home');
+});
+
+app.get('/student/create', function(req, res) {
+    res.render('pages/studentadd');
+});
+
+
+app.get('/student', function(req, res) {
+    MongoClient.connect(url, options, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("students");
+        var query = {};
+        dbo.collection("StudentInformations").find(query).toArray(function(err, result) {
+            if (err) throw err;
+            //console.log(result);
+            res.render('pages/student', { student: result });
+            db.close();
+        });
+    });
+});
+
+
+app.get("/student/detail/:sid", function(req, res) {
+    var pid = req.params.sid;
+    //Get the class detail from mongodb
+    MongoClient.connect(url, options, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("students");
+        var query = {
+            sid: pid
+        };
+        dbo.collection("StudentInformations").findOne(query, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            res.render('pages/studentdetail', { detail: result });
+            db.close();
+        });
+    });
+});
+
+app.get("/student/edit/:sid", function(req, res) {
+    var pid = req.params.sid;
+    //Get the class detail from mongodb
+    MongoClient.connect(url, options, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("students");
+        var query = {
+            sid: pid
+        };
+        dbo.collection("StudentInformations").findOne(query, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            res.render('pages/studentedit', { detail: result });
+            db.close();
+        });
+    });
+});
+
+app.post('/studentsave', function(req, res) {
+    var sID = req.body.sid;
+    var sName = req.body.name;
+    var sMajor = req.body.major;
+    var sFac = req.body.faculty;
+    var sSub = req.body.subject;
+
+    MongoClient.connect(url, options, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("students");
+        var query = {
+            sid: sID
+        }
+        var newvalues = {
+            $set: {
+                name: sName,
+                major: sMajor,
+                faculty: sFac,
+                subject: sSub,
+
+            }
+        };
+        dbo.collection('StudentInformations')
+            .updateOne(query, newvalues, function(err, result) {
+                if (err) throw err;
+                console.log("One document updated");
+                db.close();
+                res.redirect("/student");
+            });
+    });
+});
+
+app.post('/studentadd', function(req, res) {
+    var sID = req.body.sid;
+    var sName = req.body.name;
+    var sMajor = req.body.major;
+    var sFac = req.body.faculty;
+    var sSub = req.body.subject;
+    MongoClient.connect(url, options, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("students");
+        var query = {};
+        var newClass = {
+            sid: sID,
+            name: sName,
+            major: sMajor,
+            faculty: sFac,
+            subject: sSub
+        };
+        dbo.collection("StudentInformations")
+            .insertOne(newClass, function(err, result) {
+                if (err) throw err;
+                console.log("One document inserted");
+                db.close();
+                res.redirect("/student");
+            });
+    });
+});
+
+
+
+app.get("/student/delete/:sid", function(req, res) {
+    var pid = req.params.sid;
+    //Get the class detail from mongodb
+    MongoClient.connect(url, options, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("students");
+        var myquery = {
+            sid: pid
+        };
+        dbo.collection("StudentInformations").deleteOne(myquery, function(err, obj) {
+            if (err) throw err;
+            console.log("One document deleted");
+            res.redirect("/student");
+            db.close();
+        });
+    });
+});
+
+app.listen(8080);
+console.log('Express started at http://localhost:8080');
